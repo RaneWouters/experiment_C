cf02bc3a44bc302084727bfb79c3bc4819296a47
RaneWouters <1540656951@qq.com>
2020-04-01 18:33:42 +0800

    4_5.cpp update

--- 4_5.cpp
+++ 4_5.cpp
@@ -2,6 +2,7 @@
 #include<cstdio>
 #include<cstring>
 #include<vector>
+#include<algorithm>
 #include<bitset>    //bitset<8>(c)
 #include<typeinfo>  //typeid(i).name()
 using namespace std;
@@ -12,6 +13,22 @@ int joseph(int n, int m){
 int main(){
 	int m,n;
     cin>>n>>m;
-    cout<<joseph(n, m) +1;
+//    cout<<joseph(n, m) +1;
+    vector<bool> s(n, 1);
+    int dead(0), cnt(0);
+    auto it=s.begin();
+    while(dead < n-1){
+        if(it == s.end())   it = s.begin();
+        if(*it) cnt++;
+        if(cnt == m){
+            *it = 0;
+            cnt = 0;
+            dead++;
+        }
+        it++;
+    }
+    cout<<&it<<endl;
+    it=find(s.begin(), s.end(), 1);
+    cout<<it-s.begin()+1;
 	return 0;
 }
